a083d1081878ebdec3eebbfd333488bf062c2f5a
Niccolo <niccolosottile@outlook.com>
2022-02-24 16:15:09 +0000

    commit on 'master'

---
+++ question_1.c
@@ -0,0 +1,10 @@
+// Returns how many Fridays fell on the fifth of the month during the nineteenth and twentieth centuries (1 Jan 1801 to 31 Dec 2000) given the dates in the question text
+int howManyDays() {
+    
+}
+
+// Start from the given date (24 March 2002 is a Wednesday) and go back in time
+// Start counting Fridays on the 5th of the month from 31 December 2000 to 1 Jan 1801
+// Use the rules for month length (Might store in dictionary), with exception of leap years to be checked for arithmetically
+// Leap year must be divisible by 4 or if century by 400, return total count found.
+
